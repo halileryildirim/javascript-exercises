2c10dec836ec2bb20e1c0e67e38978d5c6595ec9
halileryildirim <halileryildirim@gmail.com>
2023-07-17 21:00:30 +0300

    Added solution for reverse string exercise.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    
+    let splitString = word.split("");
+    let reversedString = splitString.reverse();
+    return reversedString.join("");
 };
 
 // Do not edit below this line
