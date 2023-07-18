a558f43ba6d3e69fc53bdb027746d10d5b0dc1ff
halileryildirim <halileryildirim@gmail.com>
2023-07-18 16:51:08 +0300

    Added solution for leap years exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    if(year % 100 == 0) {
+        if (year % 25 == 0 && year % 16 == 0) {     //Divisibility for 400 is decided by testing highest power of prime factors divisibility which is 5^2 and 4^2
+            return true;
+        }
+        else
+            return false;
+    }
+    else
+        if (year % 4 == 0) {
+            return true;
+        }
+        else
+        return false;
 
 };
 
