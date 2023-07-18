7999d4c2cd44fa96f184f1eac19074e90b532019
halileryildirim <halileryildirim@gmail.com>
2023-07-18 16:38:06 +0300

    Added solution for sum all exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(num1 < 0 || num2< 0 || !Number.isFinite(num1)|| !Number.isFinite(num2)) {
+        return "ERROR";
+    }
+
+    let smallNum = 0;
+    let largeNum = 0; 
+
+    if(num1>num2) {
+        smallNum = num2;
+        largeNum = num1;
+    }
+    else if (num1<num2) {
+        smallNum = num1;
+        largeNum = num2;
+    }
+    
+    let sum = 0;
+    for (let i = smallNum; i <= largeNum; i++) {
+        sum += i;
+    }
+    return sum; 
 
 };
 
