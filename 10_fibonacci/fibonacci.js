85d74defad7f531757307b93526c14b923ecff16
halileryildirim <halileryildirim@gmail.com>
2023-07-26 20:12:47 +0300

    added solution for fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let realNum = parseInt(num);
+    let arr = new Array();
+    if(realNum<0) {
+        return 'OOPS';
+    }
 
+    for(let i = 0; i<realNum; i++) {
+        if(i == 0 || i == 1) {
+            arr.push(1);
+        }
+        else {
+            let value = arr[i-1] + arr[i-2];
+            arr.push(value);
+        }
+    }
+
+    
+    
+    return parseInt(arr.slice(-1));
 };
 
 // Do not edit below this line
