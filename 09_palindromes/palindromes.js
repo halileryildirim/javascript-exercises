f8f6c24e8f456da89cedc8985311efcc24c704e2
halileryildirim <halileryildirim@gmail.com>
2023-07-26 19:29:35 +0300

    Added solution for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let lowerStr = str.toLowerCase();
+    let actualStr = lowerStr.replaceAll(/[! ,.]/g,"");
+    let arrayString = Array.from(actualStr);
+    let reverseArray = arrayString.reverse();
+    let reverseStr = reverseArray.join("");
+    
+    if(reverseStr == actualStr){
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
