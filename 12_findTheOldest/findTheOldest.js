6aa184c14547891d19883310a6a9a21c187a611c
halileryildirim <halileryildirim@gmail.com>
2023-07-27 16:43:24 +0300

    Added solution for find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
 
+const findTheOldest = function(arr) { 
+        return arr.reduce((oldestPerson, nextPerson) => {
+            const oldestAge = age(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath); 
+            const nextAge = age(nextPerson.yearOfBirth, nextPerson.yearOfDeath);
+
+            return oldestAge < nextAge ? nextPerson:oldestPerson; 
+        });
+};
+
+const age = function(birth, death){
+    if(!death) {
+        death = new Date().getFullYear();
+        
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
